9509a17f72232a786b9c867b8ded208b366dbad3
Piotr Szczygieł <szczygiel.p@gmail.com>
2022-07-30 16:50:00 +0200

    Fix some colloquialisms, merge browsers inside plots

--- plots/plot.py
+++ plots/plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 
-width = 5.23724  # inches
+PAGE_WIDTH = 5.23724  # inches
 
 matplotlib.use("pgf")
 matplotlib.rcParams.update(
@@ -14,108 +14,94 @@ matplotlib.rcParams.update(
     }
 )
 
-colors = {
-    "JavaScript": "#efd81d",
-    "AssemblyScript": "#0076c6",
-    "Rust": "#ef4900",
-    "Zig": "#ef9f1c",
-}
-
-
-def bar_plot(xs, ys, color, ylabel, filename, fmt="%s", log=False):
-    ys_pos = range(len(ys))
-    fig, ax = plt.subplots(figsize=(width, 3))
-    ax.set_ymargin(0.1)
-    bars = ax.bar(ys_pos, ys, color=color)
-    ax.bar_label(bars, padding=2, fmt=fmt)
-    ax.set_xticks(ys_pos, xs)
-
-    if log:
-        ax.set_yscale("log")
-
-    ax.set(ylabel=ylabel)
-    fig.savefig(filename)
-    plt.close(fig)
-
 
 def benchmark():
     chrome = pd.read_csv("benchmark_chrome.csv")
     firefox = pd.read_csv("benchmark_firefox.csv")
 
-    def plot_type(df, type):
-        data = df.loc[df["type"] == type]
+    def plot_type(type):
+        data_chrome = chrome.loc[chrome["type"] == type]
+        data_firefox = firefox.loc[firefox["type"] == type]
 
-        if df is chrome:
-            file_suffix = "chrome"
-        elif df is firefox:
-            file_suffix = "firefox"
+        bar_width = 0.35
 
-        bar_plot(
-            data["language"],
-            data["time"],
-            [colors[lang] for lang in data["language"]],
-            "Czas wykonania (ms)",
-            f"{type}_{file_suffix}.pgf",
-            fmt="%d ms",
-        )
+        fig, ax = plt.subplots(figsize=(PAGE_WIDTH, 3))
+        ax.set_ymargin(0.1)
 
-    plot_type(chrome, "sort6")
-    plot_type(firefox, "sort6")
+        ticks_x, ticks_val = [], []
 
-    plot_type(chrome, "fib40")
-    plot_type(firefox, "fib40")
+        for i, (language, time) in enumerate(zip(data_chrome["language"], data_chrome["time"])):
+            bars = ax.bar(i, time, bar_width, color="#4285f4", label="Google Chrome")
+            ax.bar_label(bars, padding=2, fmt="%d ms", size="x-small")
+            ticks_x.append(i + bar_width / 2)
+            ticks_val.append(language)
 
+        for i, (language, time) in enumerate(zip(data_firefox["language"], data_firefox["time"])):
+            bars = ax.bar(i + bar_width, time, bar_width, color="orange", label="Mozilla Firefox")
+            ax.bar_label(bars, padding=2, fmt="%d ms", size="x-small")
 
-def nbody():
-    bar_plot(
-        ["JavaScript", "Rust"],
-        [5.9, 6.4],
-        "Średni czas jednego kroku symulacji (ms)",
-        "nbody_chrome.pgf",
-        fmt="%.1f ms",
-    )
+        ax.set_xticks(ticks_x, ticks_val)
+        ax.set(ylabel="Czas wykonania (ms)")
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys())
+        fig.savefig(f"{type}.pgf")
+        plt.close(fig)
 
-    bar_plot(
-        ["JavaScript", "Rust"],
-        [6.3, 6.5],
-        "Średni czas jednego kroku symulacji (ms)",
-        "nbody_firefox.pgf",
-        fmt="%.1f ms",
-    )
+    plot_type("sort6")
+    plot_type("fib40")
 
 
-def matrix():
-    bar_plot(
-        ["JavaScript", "Rust", "Rust SIMD"],
-        [994, 417, 365],
-        "Czas wykonania (ms)",
-        "matrix_chrome.pgf",
-        fmt="%d ms",
-    )
+# def nbody():
+#     bar_plot(
+#         ["JavaScript", "Rust"],
+#         [5.9, 6.4],
+#         "Średni czas jednego kroku symulacji (ms)",
+#         "nbody_chrome.pgf",
+#         fmt="%.1f ms",
+#     )
 
-    bar_plot(
-        ["JavaScript", "Rust", "Rust SIMD"],
-        [1171, 534, 458],
-        "Czas wykonania (ms)",
-        "matrix_firefox.pgf",
-        fmt="%d ms",
-    )
+#     bar_plot(
+#         ["JavaScript", "Rust"],
+#         [6.3, 6.5],
+#         "Średni czas jednego kroku symulacji (ms)",
+#         "nbody_firefox.pgf",
+#         fmt="%.1f ms",
+#     )
 
 
-def edges():
-    bar_plot(
-        [
-            "JS (Chrome)",
-            "JS (Firefox)",
-            "WASM (Chrome)",
-            "WASM (Firefox)",
-            "Rust (Native)",
-        ],
-        [51.11, 14.93, 7.43, 9.57, 0.609476],
-        "Czas procesowania klatki (ms)",
-        "edges.pgf",
-        fmt="%.2f ms",
-    )
+# def matrix():
+#     bar_plot(
+#         ["JavaScript", "Rust", "Rust SIMD"],
+#         [994, 417, 365],
+#         "Czas wykonania (ms)",
+#         "matrix_chrome.pgf",
+#         fmt="%d ms",
+#     )
+
+#     bar_plot(
+#         ["JavaScript", "Rust", "Rust SIMD"],
+#         [1171, 534, 458],
+#         "Czas wykonania (ms)",
+#         "matrix_firefox.pgf",
+#         fmt="%d ms",
+#     )
+
+
+# def edges():
+#     bar_plot(
+#         [
+#             "JS (Chrome)",
+#             "JS (Firefox)",
+#             "WASM (Chrome)",
+#             "WASM (Firefox)",
+#             "Rust (Native)",
+#         ],
+#         [51.11, 14.93, 7.43, 9.57, 0.609476],
+#         "Czas procesowania klatki (ms)",
+#         "edges.pgf",
+#         fmt="%.2f ms",
+#     )
 
 
 benchmark()
